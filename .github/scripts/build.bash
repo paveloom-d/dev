00a48bddced78530b320822a2f7d8ca411bc7d23
Pavel Sobolev <paveloom@mail.ru>
2020-06-28 18:22:30 +0000

    Add docker test for Travis build. Also, change the logs color.

--- .github/scripts/build.bash
+++ .github/scripts/build.bash
@@ -3,15 +3,15 @@
 # A script to build the image and test a container based on it
 
 # Download dive
-printf "\nDownloading dive...\n\n"
+echo -e ']n\e[1m\033[36mDownloading dive...\033[0m\n'
 wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
 
 # Install dive
-printf "Installing dive...\n\n"
+echo -e ']n\e[1m\033[36mInstalling dive...\033[0m\n'
 sudo apt install ./dive_0.9.2_linux_amd64.deb
 
 # Activate experimental features
-printf "\nActivating experimental features...\n"
+echo -e ']n\e[1m\033[36mActivating experimental features...\033[0m\n'
 sudo tee /etc/docker/daemon.json > /dev/null << EOF
 {
     "experimental": true
@@ -20,25 +20,33 @@ EOF
 sudo service docker restart
 
 # Build the image
-printf "\nBuilding the image...\n\n"
+echo -e ']n\e[1m\033[36mBuilding the image...\033[0m\n'
 docker build -t image --squash .
 
 # Analyse the image
-printf "\nAnalyzing the image...\n\n"
+echo -e ']n\e[1m\033[36mAnalyzing the image...\033[0m\n'
 CI=true dive image
 
 # Delete unnecessary build images
-printf "Building the image...\n\n"
+echo -e ']n\e[1m\033[36mBuilding the image...\033[0m\n'
 docker images -q | grep -E -v "$(docker images -q 'image')|$(docker images -q 'alpine')" | xargs docker rmi
 
 # Run a container
-printf "\nRunning a container...\n\n"
+echo -e ']n\e[1m\033[36mRunning a container...\033[0m\n'
 docker run --name container -d -t image
 
 # Test if Zsh is installed
-printf "\nTesting if Zsh is installed...\n\n"
+echo -e ']n\e[1m\033[36mTesting if Zsh is installed...\033[0m\n'
 docker exec container zsh --version
 
+# Test if docker is installed (also check permissions)
+echo -e ']n\e[1m\033[36mTesting if docker is installed...\033[0m\n'
+docker exec container docker version
+
+# Run hello-world in docker (also check permissions)
+echo -e ']n\e[1m\033[36mRunning hello-world in docker...\033[0m\n'
+docker exec container docker run hello-world
+
 # Stop the container
-printf "\nStopping the container...\n\n"
+echo -e ']n\e[1m\033[36mStopping the container...\033[0m\n'
 docker stop container
