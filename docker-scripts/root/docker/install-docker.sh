4e8a8a2dd06be03866570dba1c83bc01ad0f3a20
Pavel Sobolev <paveloom@mail.ru>
2020-07-08 16:30:08 +0000

    docker --> Docker

--- docker-scripts/root/docker/install-docker.sh
+++ docker-scripts/root/docker/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Print info
-echo '\n\e[1;36mInstalling docker:\e[0m'
+echo '\n\e[1;36mInstalling Docker:\e[0m'
 
 # Update lists of packages
 echo '\e[1;36m> Updating lists of packages...\e[0m'
