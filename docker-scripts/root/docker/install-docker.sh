59dc6b6ae331caf3f1b0b29b96464a27cdcd0ceb
Pavel Sobolev <paveloom@mail.ru>
2020-08-15 17:52:03 +0000

    Delete redundant comments and make a couple of corrections.

--- docker-scripts/root/docker/install-docker.sh
+++ docker-scripts/root/docker/install-docker.sh
@@ -1,28 +1,21 @@
 #!/bin/sh
 
-# Print info
 echo '\n\e[1;36mInstalling Docker:\e[0m'
 
-# Update lists of packages
 echo '\e[1;36m> Updating lists of packages...\e[0m'
 apt-get update >/dev/null
 
-# Add docker's official GPG key
-echo "\e[1;36m> Addding docker's official GPG key...\e[0m"
+echo "\e[1;36m> Addding Docker's official GPG key...\e[0m"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - >/dev/null
 
-# Set up stable repository
-echo '\e[1;36m> Setting up stable repository...\e[0m'
+echo '\e[1;36m> Setting up the stable repository...\e[0m'
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >/dev/null
 
-# Install Docker engine
 echo '\e[1;36m> Installing Docker engine...\e[0m'
 apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io >/dev/null
 
-# Add the user to the docker group
-echo '\e[1;36m> Adding the user to the docker group...\e[0m'
+echo '\e[1;36m> Adding the user to the `docker` group...\e[0m'
 usermod -aG docker $USER
 
-# Clean the apt cache
-echo '\e[1;36m> Cleaning the apt cache...\e[0m\n'
+echo '\e[1;36m> Cleaning the `apt` cache...\e[0m\n'
 rm -rf /var/lib/apt/lists/*
--- docker-scripts/root/essential-packages/install-essential-packages.sh
+++ docker-scripts/root/essential-packages/install-essential-packages.sh
@@ -1,83 +1,63 @@
 #!/bin/sh
 
-# Print info
 echo '\n\e[1;36mInstalling essential packages:\e[0m'
 
-# Update lists of packages
 echo '\e[1;36m> Updating lists of packages...\e[0m'
 apt-get update >/dev/null
 
-# Install apt-utils
-echo '\e[1;36m> Installing apt-utils...\e[0m'
+echo '\e[1;36m> Installing `apt-utils`...\e[0m'
 apt-get -y install apt-utils >/dev/null 2>&1
 
-# Install apt-transport-https
-echo '\e[1;36m> Installing apt-transport-https...\e[0m'
+echo '\e[1;36m> Installing `apt-transport-https`...\e[0m'
 apt-get install -y --no-install-recommends apt-transport-https >/dev/null
 
-# Install ca-certificates
-echo '\e[1;36m> Installing ca-certificates...\e[0m'
+echo '\e[1;36m> Installing `ca-certificates`...\e[0m'
 apt-get install -y --no-install-recommends ca-certificates >/dev/null
 
-# Install git
-echo '\e[1;36m> Installing git...\e[0m'
+echo '\e[1;36m> Installing `git`...\e[0m'
 apt-get install -y --no-install-recommends git >/dev/null
 
-# Install make
-echo '\e[1;36m> Installing make...\e[0m'
+echo '\e[1;36m> Installing `make`...\e[0m'
 apt-get install -y --no-install-recommends make >/dev/null
 
-# Install ncdu
-echo '\e[1;36m> Installing ncdu...\e[0m'
+echo '\e[1;36m> Installing `ncdu`...\e[0m'
 apt-get install -y --no-install-recommends ncdu >/dev/null
 
-# Install zip and unzip
-echo '\e[1;36m> Installing zip and unzip...\e[0m'
+echo '\e[1;36m> Installing `zip` and `unzip`...\e[0m'
 apt-get install -y --no-install-recommends zip unzip >/dev/null
 
-# Install nano
-echo '\e[1;36m> Installing nano...\e[0m'
+echo '\e[1;36m> Installing `nano`...\e[0m'
 apt-get install -y --no-install-recommends nano >/dev/null
 
-# Install less
-echo '\e[1;36m> Installing less...\e[0m'
+echo '\e[1;36m> Installing `less`...\e[0m'
 apt-get install -y --no-install-recommends less >/dev/null
 
-# Install wget
-echo '\e[1;36m> Installing wget...\e[0m'
+echo '\e[1;36m> Installing `wget`...\e[0m'
 apt-get install -y --no-install-recommends wget >/dev/null
 
-# Install curl
-echo '\e[1;36m> Installing curl...\e[0m'
+echo '\e[1;36m> Installing `curl`...\e[0m'
 apt-get install -y --no-install-recommends curl >/dev/null
 
-# Install gnupg-agent
-echo '\e[1;36m> Installing gnupg-agent...\e[0m'
+echo '\e[1;36m> Installing `gnupg-agent`...\e[0m'
 apt-get install -y --no-install-recommends gnupg-agent >/dev/null
 
-# Install sudo
-echo '\e[1;36m> Installing sudo...\e[0m'
+echo '\e[1;36m> Installing `sudo`...\e[0m'
 wget --no-check-certificate https://github.com/sudo-project/sudo/releases/download/SUDO_1_9_1/sudo_1.9.1-1_ubu2004_amd64.deb >/dev/null 2>&1
 apt-get -y install ./sudo_1.9.1-1_ubu2004_amd64.deb >/dev/null
 rm sudo_1.9.1-1_ubu2004_amd64.deb
 
-# Install ssh
-echo '\e[1;36m> Installing ssh...\e[0m'
+echo '\e[1;36m> Installing `ssh`...\e[0m'
 apt-get install -y --no-install-recommends ssh >/dev/null
 
-# Install keychain
-echo '\e[1;36m> Installing keychain...\e[0m'
+echo '\e[1;36m> Installing `keychain`...\e[0m'
 apt-get install -y --no-install-recommends keychain >/dev/null
 
-# Install locales
-echo '\e[1;36m> Installing locales...\e[0m'
+echo '\e[1;36m> Installing `locales`...\e[0m'
 apt-get install -y --no-install-recommends locales >/dev/null
 locale-gen ru_RU.UTF-8 >/dev/null
 
-# Install software-properties-common
-echo '\e[1;36m> Installing software-properties-common...\e[0m'
+echo '\e[1;36m> Installing `software-properties-common`...\e[0m'
 apt-get install -y --no-install-recommends software-properties-common >/dev/null
 
-# Clean the apt cache
-echo '\e[1;36m> Cleaning the apt cache...\e[0m\n'
+echo '\e[1;36m> Cleaning the `apt` cache...\e[0m\n'
 rm -rf /var/lib/apt/lists/*
