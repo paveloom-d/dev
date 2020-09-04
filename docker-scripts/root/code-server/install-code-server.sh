ceee133398ff484e6015629e853b7758c6529fbb
Pavel Sobolev <paveloom@mail.ru>
2020-09-04 18:59:21 +0300

    Hide the output.

--- docker-scripts/root/code-server/install-code-server.sh
+++ docker-scripts/root/code-server/install-code-server.sh
@@ -6,10 +6,10 @@ PACKAGE_TAG=3.5.0
 PACKAGE_NAME=code-server_${PACKAGE_TAG}_amd64.deb
 
 echo '\e[1;36m> Getting the package...\e[0m'
-curl -fOL https://github.com/cdr/code-server/releases/download/v$PACKAGE_TAG/$PACKAGE_NAME #>/dev/null 2>&1
+curl -fOL https://github.com/cdr/code-server/releases/download/v$PACKAGE_TAG/$PACKAGE_NAME >/dev/null 2>&1
 
 echo '\e[1;36m> Installing the package...\e[0m'
-dpkg -i $PACKAGE_NAME #>/dev/null
+dpkg -i $PACKAGE_NAME >/dev/null
 
 echo '\e[1;36m> Deleting the package file...\e[0m'
 rm $PACKAGE_NAME
