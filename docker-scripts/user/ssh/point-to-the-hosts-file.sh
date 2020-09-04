452f859d0c6742df6b0b65462be96be09ce6b4af
Pavel Sobolev <paveloom@mail.ru>
2020-09-04 22:23:12 +0300

    Create the `~/.ssh` directory.

--- docker-scripts/user/ssh/point-to-the-hosts-file.sh
+++ docker-scripts/user/ssh/point-to-the-hosts-file.sh
@@ -3,4 +3,5 @@
 echo '\n\e[1;36mPointing to the hosts file for SSH:\e[0m'
 
 echo '\e[1;36m> Creating `~/.ssh/config`...\e[0m\n'
+mkdir -p ~/.ssh
 echo 'UserKnownHostsFile ~/.ssh/known_hosts' > ~/.ssh/config
