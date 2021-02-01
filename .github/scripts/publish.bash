cf5b6132a2fc54667059b32571dde7289596008c
Pavel Sobolev <paveloom@mail.ru>
2021-02-01 13:38:23 +0000

    Finish the rewrite; test on the `update` branch.

--- .github/scripts/publish.bash
+++ .github/scripts/publish.bash
@@ -3,15 +3,15 @@
 # A script to build and push the image
 
 # Build the image
-podman build --squash-all -t dev .
+podman build --squash-all -t $IMAGENAME .
 
 # Login to the GHCR
 echo $GHCR_PAT | podman login ghcr.io -u $USERNAME --password-stdin
 
 # Push the latest version
-podman push dev ghcr.io/$REPOSITORY:latest
+podman push $IMAGENAME ghcr.io/$OWNER/$IMAGENAME:latest
 
 # Push the release version
-if [ "$PUBLISH_RELEASE_VERSION" -eq "true" ]; then
-    podman push dev ghcr.io/$REPOSITORY:${{ env.RELEASE_VERSION }}
+if [ "$PUBLISH_RELEASE_VERSION" = true ]; then
+    podman push dev ghcr.io/$OWNER/$IMAGENAME:${{ env.RELEASE_VERSION }}
 fi
