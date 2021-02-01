10623ac8e574da13362c6d69745f40c36bb9aaa0
Pavel Sobolev <paveloom@mail.ru>
2021-02-01 15:46:37 +0000

    Add missing commands when installing Midori browser.

--- build-scripts/root/browser/install-browser.sh
+++ build-scripts/root/browser/install-browser.sh
@@ -2,5 +2,11 @@
 
 echo '\n\e[1;36mInstalling the browser:\e[0m'
 
-echo '\e[1;36m> Installing Midori:\e[0m\n'
+echo '\e[1;36m> Updating lists of packages...\e[0m'
+apt-get update >/dev/null
+
+echo '\e[1;36m> Installing Midori...\e[0m\n'
 apt-get install -y --no-install-recommends midori >/dev/null
+
+echo '\e[1;36m> Cleaning the `apt` cache...\e[0m\n'
+rm -rf /var/lib/apt/lists/*
