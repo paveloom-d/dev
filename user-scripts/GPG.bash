89939319cdf0ec81cc85623bffe30cfe9b16fd0e
Pavel Sobolev <Paveloom@mail.ru>
2021-02-03 16:06:42 +0300

    (#130) Remove an extra line in the output of the GPG user-script.

--- user-scripts/GPG.bash
+++ user-scripts/GPG.bash
@@ -77,7 +77,7 @@ git config --global pull.rebase false
 git config --global commit.gpgsign true
 git config --global gpg.program gpg
 
-echo -e "\n${cyan}This script have also updated the ${mage}git${cyan} configuration. You can check it using ${mage}git config --list${cyan}.${reset}\n"
+echo -e "\n${cyan}This script have also updated the ${mage}git${cyan} configuration. You can check it using ${mage}git config --list${cyan}.${reset}"
 
 # Add several Git aliases
 git config --global alias.new '!f() {
