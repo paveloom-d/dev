a371030800bced8473ff29c613488ec2bae46dbc
Pavel Sobolev <paveloom@mail.ru>
2021-04-22 19:49:11 +0000

    Try installing some other essential packages.

--- build-scripts/root/install-essential-packages.bash
+++ build-scripts/root/install-essential-packages.bash
@@ -11,9 +11,8 @@ echo -e '\n\e[1;36mInstalling essential packages:\e[0m'
 # echo -e '\e[1;36m> Installing `apt-transport-https`...\e[0m'
 # apt-get install -y --no-install-recommends apt-transport-https >/dev/null
 
-echo -e '\e[1;36m> Installing `build-essential`...\e[0m'
+# echo -e '\e[1;36m> Installing `build-essential`...\e[0m'
 # apt-get install -y --no-install-recommends build-essential >/dev/null
-install_packages build-essential
 
 # echo -e '\e[1;36m> Installing `dialog`...\e[0m'
 # apt-get install -y --no-install-recommends dialog >/dev/null
@@ -27,8 +26,9 @@ install_packages build-essential
 # echo -e '\e[1;36m> Installing `ca-certificates`...\e[0m'
 # apt-get install -y --no-install-recommends ca-certificates >/dev/null
 
-# echo -e '\e[1;36m> Installing `git`...\e[0m'
+echo -e '\e[1;36m> Installing `git`...\e[0m'
 # apt-get install -y --no-install-recommends git >/dev/null
+install_packages git
 
 # echo -e '\e[1;36m> Installing `make`...\e[0m'
 # apt-get install -y --no-install-recommends make >/dev/null
@@ -36,44 +36,56 @@ install_packages build-essential
 # echo -e '\e[1;36m> Installing `screen`...\e[0m'
 # apt-get install -y --no-install-recommends screen >/dev/null
 
-# echo -e '\e[1;36m> Installing `ncdu`...\e[0m'
+echo -e '\e[1;36m> Installing `ncdu`...\e[0m'
 # apt-get install -y --no-install-recommends ncdu >/dev/null
+install_packages ncdu
 
-# echo -e '\e[1;36m> Installing `zip` and `unzip`...\e[0m'
+echo -e '\e[1;36m> Installing `zip` and `unzip`...\e[0m'
 # apt-get install -y --no-install-recommends zip unzip >/dev/null
+install_packages zip unzip
 
-# echo -e '\e[1;36m> Installing `nano`...\e[0m'
+echo -e '\e[1;36m> Installing `nano`...\e[0m'
 # apt-get install -y --no-install-recommends nano >/dev/null
+install_packages nano
 
-# echo -e '\e[1;36m> Installing `less`...\e[0m'
+echo -e '\e[1;36m> Installing `less`...\e[0m'
 # apt-get install -y --no-install-recommends less >/dev/null
+install_packages less
 
-# echo -e '\e[1;36m> Installing `wget`...\e[0m'
+echo -e '\e[1;36m> Installing `wget`...\e[0m'
 # apt-get install -y --no-install-recommends wget >/dev/null
+install_packages wget
 
-# echo -e '\e[1;36m> Installing `curl`...\e[0m'
+echo -e '\e[1;36m> Installing `curl`...\e[0m'
 # apt-get install -y --no-install-recommends curl >/dev/null
+install_packages curl
 
-# echo -e '\e[1;36m> Installing `gnupg-agent`...\e[0m'
+echo -e '\e[1;36m> Installing `gnupg-agent`...\e[0m'
 # apt-get install -y --no-install-recommends gnupg-agent >/dev/null
+install_packages gnupg-agent
 
-# echo -e '\e[1;36m> Installing `sudo`...\e[0m'
-# wget --no-check-certificate https://github.com/sudo-project/sudo/releases/download/SUDO_1_9_1/sudo_1.9.1-1_ubu2004_amd64.deb >/dev/null 2>&1
+echo -e '\e[1;36m> Installing `sudo`...\e[0m'
+# wget --no-check-certificate https://github.com/sudo-project/sudo/releases/download/SUDO_1_9_6p1/sudo_1.9.6-2_deb10_amd64.deb >/dev/null 2>&1
 # apt-get -y install ./sudo_1.9.1-1_ubu2004_amd64.deb >/dev/null
 # rm sudo_1.9.1-1_ubu2004_amd64.deb
+install_packages sudo
 
-# echo -e '\e[1;36m> Installing `ssh`...\e[0m'
+echo -e '\e[1;36m> Installing `ssh`...\e[0m'
 # apt-get install -y --no-install-recommends ssh >/dev/null
+install_packages ssh
 
-# echo -e '\e[1;36m> Installing `keychain`...\e[0m'
+echo -e '\e[1;36m> Installing `keychain`...\e[0m'
 # apt-get install -y --no-install-recommends keychain >/dev/null
+install_packages keychain
 
-# echo -e '\e[1;36m> Installing `locales`...\e[0m'
+echo -e '\e[1;36m> Installing `locales`...\e[0m'
 # apt-get install -y --no-install-recommends locales >/dev/null
-# locale-gen ru_RU.UTF-8 >/dev/null
+install_packages locales
+locale-gen ru_RU.UTF-8 >/dev/null
 
-# echo -e '\e[1;36m> Installing `language-pack-en-base`...\e[0m'
+echo -e '\e[1;36m> Installing `language-pack-en-base`...\e[0m'
 # apt-get install -y --no-install-recommends language-pack-en-base >/dev/null
+install_packages language-pack-en-base
 
 # echo -e '\e[1;36m> Installing `software-properties-common`...\e[0m'
 # apt-get install -y --no-install-recommends software-properties-common >/dev/null
