5d55df17d9bce720c3e3a0c06e6ed0f1a8c17062
Pavel Sobolev <paveloom@mail.ru>
2021-04-22 20:07:16 +0000

    Add `htop`.

--- build-scripts/root/install-essential-packages.bash
+++ build-scripts/root/install-essential-packages.bash
@@ -20,15 +20,15 @@ echo -e '\n\e[1;36mInstalling essential packages:\e[0m'
 # echo -e '\e[1;36m> Installing `dumb-init`...\e[0m'
 # apt-get install -y --no-install-recommends dumb-init >/dev/null
 
-# echo -e '\e[1;36m> Installing `htop`...\e[0m'
+echo -e '\e[1;36m> Installing `htop`...\e[0m'
 # apt-get install -y --no-install-recommends htop >/dev/null
+install_packages htop
 
 # echo -e '\e[1;36m> Installing `ca-certificates`...\e[0m'
 # apt-get install -y --no-install-recommends ca-certificates >/dev/null
 
 echo -e '\e[1;36m> Installing `git`...\e[0m'
-# apt-get install -y --no-install-recommends git >/dev/null
-install_packages git
+install_packages git >/dev/null
 
 # echo -e '\e[1;36m> Installing `make`...\e[0m'
 # apt-get install -y --no-install-recommends make >/dev/null
@@ -38,49 +38,40 @@ install_packages git
 
 echo -e '\e[1;36m> Installing `ncdu`...\e[0m'
 # apt-get install -y --no-install-recommends ncdu >/dev/null
-install_packages ncdu
+install_packages ncdu >/dev/null
 
 echo -e '\e[1;36m> Installing `zip` and `unzip`...\e[0m'
-# apt-get install -y --no-install-recommends zip unzip >/dev/null
-install_packages zip unzip
+install_packages zip unzip >/dev/null
 
 echo -e '\e[1;36m> Installing `nano`...\e[0m'
-# apt-get install -y --no-install-recommends nano >/dev/null
-install_packages nano
+install_packages nano >/dev/null
 
 echo -e '\e[1;36m> Installing `less`...\e[0m'
-# apt-get install -y --no-install-recommends less >/dev/null
-install_packages less
+install_packages less >/dev/null
 
 echo -e '\e[1;36m> Installing `wget`...\e[0m'
-# apt-get install -y --no-install-recommends wget >/dev/null
-install_packages wget
+install_packages wget >/dev/null
 
 echo -e '\e[1;36m> Installing `curl`...\e[0m'
-# apt-get install -y --no-install-recommends curl >/dev/null
-install_packages curl
+install_packages curl >/dev/null
 
 echo -e '\e[1;36m> Installing `gnupg-agent`...\e[0m'
-# apt-get install -y --no-install-recommends gnupg-agent >/dev/null
-install_packages gnupg-agent
+install_packages gnupg-agent >/dev/null
 
 echo -e '\e[1;36m> Installing `sudo`...\e[0m'
 # wget --no-check-certificate https://github.com/sudo-project/sudo/releases/download/SUDO_1_9_6p1/sudo_1.9.6-2_deb10_amd64.deb >/dev/null 2>&1
 # apt-get -y install ./sudo_1.9.1-1_ubu2004_amd64.deb >/dev/null
 # rm sudo_1.9.1-1_ubu2004_amd64.deb
-install_packages sudo
+install_packages sudo >/dev/null
 
 echo -e '\e[1;36m> Installing `ssh`...\e[0m'
-# apt-get install -y --no-install-recommends ssh >/dev/null
-install_packages ssh
+install_packages ssh >/dev/null
 
 echo -e '\e[1;36m> Installing `keychain`...\e[0m'
-# apt-get install -y --no-install-recommends keychain >/dev/null
-install_packages keychain
+install_packages keychain >/dev/null
 
 echo -e '\e[1;36m> Installing `locales`...\e[0m'
-# apt-get install -y --no-install-recommends locales >/dev/null
-install_packages locales
+install_packages locales >/dev/null
 locale-gen ru_RU.UTF-8 >/dev/null
 
 # echo -e '\e[1;36m> Installing `language-pack-en-base`...\e[0m'
