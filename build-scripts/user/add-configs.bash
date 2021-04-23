a3aeef2c76ec7b4fa9aac2d7fb9654fd86b1723e
Pavel Sobolev <paveloom@mail.ru>
2021-04-23 14:22:04 +0000

    Use `sudo`.

--- build-scripts/user/add-configs.bash
+++ build-scripts/user/add-configs.bash
@@ -7,7 +7,7 @@ mkdir -p ~/.ssh
 echo -e 'UserKnownHostsFile ~/.ssh/known_hosts' > ~/.ssh/config
 
 echo -e '\e[1;36m> Creating `/etc/wsl.conf`...\e[0m'
-echo -e '[user]
-default = paveloom' > /etc/wsl.conf
+sudo bash -c "echo -e '[user]
+default = ${USER}' > /etc/wsl.conf"
 
 echo
