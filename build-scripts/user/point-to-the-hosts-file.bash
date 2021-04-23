f35055a82a1d422827a1d0f46f23c0a9285536a8
Pavel Sobolev <Paveloom@mail.ru>
2021-04-23 17:32:15 +0300

    (#139) Add a config for WSL; set a value for `DISPLAY`.

--- build-scripts/user/point-to-the-hosts-file.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e '\n\e[1;36mPointing to the hosts file for SSH:\e[0m'
-
-echo -e '\e[1;36m> Creating `~/.ssh/config`...\e[0m'
-mkdir -p ~/.ssh
-echo -e 'UserKnownHostsFile ~/.ssh/known_hosts' > ~/.ssh/config
-
-echo
